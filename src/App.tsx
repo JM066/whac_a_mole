d239cec6ecc3f3d6a5dd1e5c66a90331a2757b74
JM066 <56789447+JM066@users.noreply.github.com>
2024-02-02 17:34:42 +0000

    Removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,9 @@ import { MixerHorizontalIcon, Cross2Icon } from "@radix-ui/react-icons"
 // import reducer from "@/reducer"
 import getLocalStorage, { removeLocalStorage, setLocalStorage } from "@/helpers/getTimer.ts"
 import Game from "@/component/Game"
-import Time from "@/component/Time"
-// import Mole from "@/component/Mole"
+// import Time from "@/component/Time"
 import Button from "@/component/Button"
-import Typography from "@/component/Typography"
+// import Typography from "@/component/Typography"
 import { LOCAL_STORAGE_KEY } from "./app.type.ts"
 import "./App.scss"
 
@@ -16,14 +15,8 @@ function App() {
   const status = getLocalStorage(LOCAL_STORAGE_KEY.IS_STARTED)
   const [isStarted, setIsStarted] = useState<boolean>(status != null ? JSON.parse(status) : false)
 
-  // const handleClick = (index: number) => {
-  //   dispatch({ type: "hit", index })
-  // }
   const handleStart = () => {
     setIsStarted((prev) => !prev)
-    // removeLocalStorage(LOCAL_STORAGE_KEY.TIME);
-    // setLocalStorage(LOCAL_STORAGE_KEY.IS_STARTED, JSON.stringify(true));
-    // setIsStarted(true)
   }
 
   const addScore = () => {}
